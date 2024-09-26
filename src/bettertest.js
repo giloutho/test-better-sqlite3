04dcdbb92fe4f8c11841d99af91b4cf0b444fda0
giloutho <gilouthomas@gmail.com>
2024-09-26 18:08:40 +0200

    Test a memory database

--- src/bettertest.js
+++ src/bettertest.js
@@ -1,70 +1,76 @@
 const path = require('path')
-var fs = require('fs')
+const fs = require('fs')
 
-// Delete sample.db if exists
-const pathDb = './sample.db'
+let txt = document.getElementById('connect')
+let logMsg =''
 
-if (fs.existsSync(pathDb)) {
-    try {
-        fs.unlinkSync(pathDb)
-        console.log('Existing file "sample.db" has been deleted');
-    } catch(err) {
-    console.error(err)
-    }
-}
+const Database = require('better-sqlite3')
+const db = new Database(':memory:')
 
+const sqlCreatePath = path.join(__dirname, '../ext_resources/','sample-create.sql')
+const shemaCreation = fs.readFileSync(sqlCreatePath, 'utf8')
 
-const db = require('better-sqlite3')('sample.db')
-const shemaCreation = fs.readFileSync('sample-create.sql', 'utf8')
 try {
     db.exec(shemaCreation)
-    console.log('sample.db created...')
-    const shemaData = fs.readFileSync('sample-data.sql', 'utf8')
+    console.log('In-memory database  created...')
+    displayLog('In-memory database  created...')
+    const sqlDataPath = path.join(__dirname, '../ext_resources/','sample-data.sql')
+    const shemaData = fs.readFileSync(sqlDataPath, 'utf8')
     db.exec(shemaData)
-    console.log('sample data inserted in database...')
+    displayLog('sample data inserted in database...')
     
     // request testing
-    console.log('*** Test -> SELECT * FROM employees')
+    displayLog('*** Test -> SELECT * FROM employees')
     // Compact writing, all is attached to the statement
     const allEmp = db.prepare('SELECT * FROM employees').all()
     allEmp.forEach((row) => {
-      console.log(row.first_name+' '+row.last_name)
+      displaySimple(row.first_name+' '+row.last_name)
     })
 
     // Similar to .all(), but instead of returning every row together, an iterator is returned so you can retrieve the rows one by one. 
     // If you plan on retrieving every row anyways, .all() will perform slightly better.
-    console.log('*** Test -> iterator usage')
+    displayLog('*** Test -> iterator usage')
     const allCount = db.prepare('SELECT * FROM countries')
     for (var row of allCount.iterate()) {
       // id 3 for Asia
       if (row.region_id === 3) {     
-        console.log(row.country_id+' '+row.country_name);
+        displayLog(row.country_id+' '+row.country_name);
         break;
       }
     }    
 
     // We wait for a single line, we use get which returns only the first line
-    console.log('*** Test -> result returns only the first line')
+    displayLog('*** Test -> result returns only the first line')
     const oneEmpl = db.prepare('SELECT * FROM employees WHERE last_name = \'Khoo\'').get()
-    console.log(oneEmpl.first_name+' '+oneEmpl.last_name+' '+oneEmpl.salary)
+    displayLog(oneEmpl.first_name+' '+oneEmpl.last_name+' '+oneEmpl.salary)
 
-    console.log('*** Test -> displays the number of employees with a salary above 10,000')
+    displayLog('*** Test -> displays the number of employees with a salary above 10,000')
     const maxSal = db.prepare('SELECT count(*) as nbmaxsal FROM employees WHERE salary > 10000').get()
-     console.log(maxSal.nbmaxsal)
+     displayLog(maxSal.nbmaxsal)
 
-    console.log('*** Test -> insert a record and returns last ID')
+    displayLog('*** Test -> insert a record and returns last ID')
     const insertjob = db.prepare('INSERT INTO jobs (job_title, min_salary, max_salary) VALUES (?, ?, ?)')
     const result = insertjob.run('Full-stack dev', '11000', '18000')
-    console.log('Row affected : '+result.changes+' last ID : '+result.lastInsertRowid)
+    displayLog('Row affected : '+result.changes+' last ID : '+result.lastInsertRowid)
 
-    console.log('*** Test -> delete a record and returns changes parameter')
+    displayLog('*** Test -> delete a record and returns changes parameter')
     const deleteIt = db.prepare('DELETE FROM departments WHERE department_id=6');
     const resDel =  deleteIt.run()
-    console.log(resDel)   
+    displayLog(resDel.changes)   
  
     // All changes are saved even if the database is not closed
     db.close()
-    console.log('database closed')
+    displayLog('database closed')
   } catch (err) {
     console.error(err)
+  }
+
+function displayLog(msg) {
+  logMsg += '<br/><br/>'+msg
+  txt.innerHTML = logMsg
+}
+
+function displaySimple(msg) {
+    logMsg += '<br/>'+msg
+    txt.innerHTML = logMsg
   }
